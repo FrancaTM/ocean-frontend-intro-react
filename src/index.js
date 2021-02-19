e6c136bc1fc82e7ff0dadda877b6a79a5cdb41bb
FrancaTM <francatm@outlook.com>
2021-02-18 21:40:26 -0300

    add board component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function Board() {
+  return <div>Board</div>;
+}
+
 function Game() {
-  return <div>Jogo</div>;
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+
+      <div className="game-info">Info</div>
+    </div>
+  );
 }
 
 ReactDOM.render(<Game />, document.getElementById("root"));
