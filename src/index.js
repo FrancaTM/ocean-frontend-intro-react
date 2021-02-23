c0fb6773ba1751518abb41c4a83c35f8c359eefc
FrancaTM <francatm@outlook.com>
2021-02-22 21:38:19 -0300

    add victory check

--- src/index.js
+++ src/index.js
@@ -95,5 +95,28 @@ class Game extends React.Component {
 ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
-  return true;
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let index = 0; index < lines.length; index++) {
+    const line = lines[index];
+
+    const a = line[0];
+    const b = line[1];
+    const c = line[2];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return true;
+    }
+  }
+
+  return false;
 }
